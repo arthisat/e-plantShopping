bd9d1b08ef3e356eee1b723042f4818d26001d8c
arthisat <art.sathian@gmail.com>
2025-09-03 10:44:27 -0400

    define quantity updating reducers in CartSlice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,29 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const { name, image, cost } = action.payload; // Destructure product details from the action payload
+
+        // Check if the item already exists in the cart by comparing names
+        const existingItem = state.items.find(item => item.name === name);
+        
+        if (existingItem) {
+            // If item exists, increase quantity
+            existingItem.quantity++;
+        } else {
+            // If item DNE, add to cart with quantity = 1
+            state.items.push({ name, image, cost, quantity: 1 });
+        }
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
+        const { name, quantity } = action.payload;
+        const itemToUpdate = state.items.find(item => item.name === name);
 
-    
+        if (itemToUpdate) {
+            itemToUpdate.quantity = quantity; // If the item is found, update its quantity to the new value
+        }
     },
   },
 });
