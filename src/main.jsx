708427efb6ff003b41728fa39a6da6554bc20e02
arthisat <art.sathian@gmail.com>
2025-09-17 12:42:15 -0400

    configure store and main

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
+<Provider store={store}>
+    <App />
+</Provider>
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
